a5845768479aa533a139e95e31c602a5650e7ea5
Developer-Afzal <afzalansari891@gmail.com>
2024-07-13 22:55:59 +0530

    route added

--- routes/Admin/AdminRoutes.js
+++ routes/Admin/AdminRoutes.js
@@ -4,6 +4,9 @@ const {getStudent} = require('../../Controllers/Admin')
 
  const  router = Router()
 
-router.get('/api/v1/list', getStudent)
+router.get('/', getStudent)
+router.get('/api/v1/list', (req, res)=>{
+    res.status(200).json({message:'list page'})
+})
 
 module.exports = router
