a9a90a518145cd3642755adeeca46bd149b08799
Developer-Afzal <afzalansari891@gmail.com>
2024-07-25 21:54:53 +0530

    route updated

--- controllers/studentuser_controller.js
+++ controllers/studentuser_controller.js
@@ -97,8 +97,8 @@ const checkfeeStatus = async (request, response)=>{
          event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
     } catch (error) {
         // Log the error for monitoring and debugging
-    console.error(`Webhook signature verification failed: ${err.message}`);
-    return response.status(400).send(`Webhook Error: ${err.message}`);
+    console.error(`Webhook signature verification failed: ${error}`);
+    return response.status(400).send(`Webhook Error: ${error}`);
     }
 
       // Handle the event based on its type
